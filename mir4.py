04cd1a2fa82ab2f6966dd941ce1f9f28ff4a7fc7
Fahmi Yufrizal Yusuf <60032856+fahmiyufrizal@users.noreply.github.com>
2024-03-28 00:15:17 +0700

    Add files via upload

---
+++ mir4.py
@@ -0,0 +1,20 @@
+# mir4 simple launcher
+# by fahmiyufrizal@2024
+
+import os
+import subprocess
+from os import path
+import sys
+from sys import exit
+import time
+import pathlib
+
+# parameters
+version = 'v1.0'
+titlewindow = 'fahmiyufrizal@2024 [github.com/fahmiyufrizal]'
+MIR4FN = (r'Mir4_NetCafe_Launcher.exe')
+MIR4LN = (r'Mir4Launcher\Mir4Launcher.exe')
+dp0 = os.getcwd()
+config_mir4 = path.expandvars(r'%localappdata%\Mir4Launcher')
+dataconfig = (r'_Data_\LocalAppData\Mir4Launcher')
+exit()
